dab25f40c86fb9ee5012f7950f26e7d00403b63b
e <hh_7@foxmail.com>
2018-03-14 00:36:47 +0800

    add bootstrap

--- src/main/java/com/spike/Application.java
+++ src/main/java/com/spike/Application.java
@@ -1,12 +1,15 @@
-package com.spike.controller;
+package com.spike;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableAutoConfiguration
 @MapperScan("com.spike.dao")
 public class Application {
 
